7168b7fd7937aabdee197e4fad386b5074a4747e
fspoliveira <fspoliveira@gmail.com>
2020-04-01 17:29:01 -0300

    Update PropayTimeShitApontatorTabajara.java

--- src/br/com/propay/portaldorh/com/br/time/shit/PropayTimeShitApontatorTabajara.java
+++ src/br/com/propay/portaldorh/com/br/time/shit/PropayTimeShitApontatorTabajara.java
@@ -54,8 +54,8 @@ public class PropayTimeShitApontatorTabajara {
 		System.out.println("Preenche o apontamento na data de hoje");
 		fillTimeShit();
 
-		new WebDriverWait(driver, 10)
-		.until(ExpectedConditions.visibilityOfElementLocated(By.name("Submit22")));		
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.elementToBeClickable(By.name("Submit22")));		
 		
 		driver.quit();
 	}
